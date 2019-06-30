bf351608c5f572e6cdb7f2afa1e4c52fce353c9c
saksham taneja <sakshamtaneja7861@gmail.com>
2019-06-30 06:33:06 +0530

    uploded project cafe management

---
+++ src/cafe/management/CafeManagement.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cafe.management;
+
+/**
+ *
+ * @author dell
+ */
+public class CafeManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
